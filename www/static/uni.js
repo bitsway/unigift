e4772f8a315db2e9069ed1054ddf2e9579a325d1
nadira.nuri <nadira.nuri@gmail.com>
2017-11-09 10:26:16 +0600

    t 16 20171109

--- www/static/uni.js
+++ www/static/uni.js
@@ -216,7 +216,7 @@ function giftSave(){
 		}
 		else{
 			
-			$("#gSError").text("Point should be greater than Gift POint");
+			$("#gSError").text("Point should be greater than Gift Point");
 			
 		}
 			
